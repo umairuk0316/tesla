e3ecb083b77b623638c8610be6886dcab9bf3331
umairuk0316 <umairuk0316@gmail.com>
2026-01-12 11:39:52 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const openNav = document.querySelector(".Navbar");
+const asd = document.querySelector(".top-list");
+
+openNav.addEventListener("click", () => {
+  asd.classList.toggle("active");
+});
